07a9a29bd50c73cb68b1f0539db3909d987da8ff
IzanamiiDevv <rafaeloli596@gmail.com>
2024-04-16 18:42:23 +0800

    .

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+/*
+Asynchronous activity relative to Javascript Function:
+
+Design a Web Page that will let the user input any number. Using Javascript Function, write the code that will convert the inputted number by the user into different number system such as binary, octal, and hexadecimal. the output may look like what is showing in the video clip. Take a video of yourself while doing the writing the code and executing it. then post it in any social media with the tagline "Asynchronous Activity in Computer Programming: Learning Javascript Function". tag me to check your output
+*/
+
 // 2:binary
 // 8:Octal
 // 16:Hexidacimal
